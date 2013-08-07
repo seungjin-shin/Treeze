f8dd1d200381afee3c97e1586409e4ebc32bd322
TaeUng Song <yukult400@gmail.com>
2013-08-07 13:17:32 +0900

    pc버전 학생 다시 새로 시작

--- freemind/extensions/StatefulNodeHook.java
+++
@@ -1,9 +0,0 @@
-package freemind.extensions;
-
-public interface StatefulNodeHook extends PermanentNodeHook {
-	void setContent(String key, String content);
-	String getContent(String key);
-	void setContent(String content);
-	String getContent();
-	void setContentUndoable(String key, String content);
-}
